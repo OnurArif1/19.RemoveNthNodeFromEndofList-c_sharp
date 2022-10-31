430209acc7ae2a1c72334890a47be1d62525c500
Onur Arif Ciftci <101008431+OnurArif1@users.noreply.github.com>
2022-10-31 15:33:57 +0300

    initial

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿Node RemoveNthFromEnd(Node head, int n)
+{
+    Node temp = head;
+    Node temp2 = head;
+    int check = 0;
+    while (temp != null)
+    {
+        check++;
+        temp = temp.next;
+    }
+    if (check == 1 && n == 1)
+    {
+        return null;
+    }
+    else if (check == n)
+    {
+        head = head.next;
+        return head;
+    }
+    int i = 0;
+    while (i < check - n - 1)
+    {
+        temp2 = temp2.next;
+        i++;
+    }
+    temp2.next = temp2.next.next;
+    return head;
+}
+
+
+class Node
+{
+    public int data;
+    public Node next;
+
+    public Node(int data)
+    {
+        this.data = data;
+        this.next = null;
+    }
+}
